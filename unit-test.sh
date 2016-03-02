e67b94d250b75cb4e1e31524f1f9f4fff14c383c
Joseph Longever <joseph.longever@gmail.com>
2016-03-02 13:46:07 -0500

    clear npm cache

--- unit-test.sh
+++ unit-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash +xe
 runUnitTest() {
    cd ${WORKSPACE}/build
+   npm cache clean >& /dev/null
    rm -rf ./node_modules && npm install && npm test 
-   #rm -rf ./node_modules && npm install && npm install mocha-jenkins-reporter
+   #npm cache clean && npm install && npm install mocha-jenkins-reporter
    #JUNIT_REPORT_PATH=report.xml JUNIT_REPORT_STACK=1 \
    #    ./node_modules/.bin/mocha $(find spec -name '*-spec.js') -R spec --require spec/helper.js --reporter mocha-jenkins-reporter || true 
    #cp -rf report.xml ${WORKSPACE}/xunit-reports/mocha-report.xml
