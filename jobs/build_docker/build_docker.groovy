107a1c412e014cbc09daabf42d15adc8ed6653c2
pengtian0 <Tian.Peng@emc.com>
2017-02-13 04:33:38 -0500

    change owner of docker files

--- jobs/build_docker/build_docker.groovy
+++ jobs/build_docker/build_docker.groovy
@@ -7,7 +7,7 @@ node(build_docker_node){
                 "CLONE_DIR=b"]){
 
                 deleteDir()
-
+                def current_workspace = pwd()
                 def shareMethod
                 dir("Build_Docker_JFiles"){
                     checkout scm
@@ -23,11 +23,12 @@ node(build_docker_node){
                     usernameColonPassword(credentialsId: 'a94afe79-82f5-495a-877c-183567c51e0b', 
                                           variable: 'BINTRAY_CREDS')]){
                     timeout(90){
-                        sh './Build_Docker_JFiles/jobs/build_docker/build_docker.sh'
+                        withEnv(["WORKSPACE=${current_workspace}"]){
+                            sh './Build_Docker_JFiles/jobs/build_docker/build_docker.sh'
+                        }
                     }
                 }
-
-                stash name: 'docker_build_record', includes: 'build_record'
+                env.DOCKER_WORKSPACE="${current_workspace}"
             }
         }
     }
--- jobs/build_docker/docker_post_test.groovy
+++ jobs/build_docker/docker_post_test.groovy
@@ -1,37 +1,33 @@
 node(build_docker_node){
     lock("docker"){
         timestamps{
-        
-            def shareMethod
-            dir("Docker_Post_Test_JFiles"){
-                checkout scm
-                shareMethod = load("jobs/shareMethod.groovy")
-            }
-
-            def url = "https://github.com/RackHD/on-build-config.git"
-            def branch = "master"
-            def targetDir = "build-config"
-            shareMethod.checkout(url, branch, targetDir)
+            withEnv(["WORKSPACE=${env.DOCKER_WORKSPACE}"]){
+                def shareMethod
+                dir("Docker_Post_Test_JFiles"){
+                    checkout scm
+                    shareMethod = load("jobs/shareMethod.groovy")
+                }
+                def url = "https://github.com/RackHD/on-build-config.git"
+                def branch = "master"
+                def targetDir = "build-config"
+                shareMethod.checkout(url, branch, targetDir)
   
-            unstash "docker_build_record"
+                withCredentials([
+                    usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
+                                     passwordVariable: 'SUDO_PASSWORD',
+                                     usernameVariable: 'SUDO_USER')]
+                ){
+                    timeout(90){
+                        sh '''#!/bin/bash +xe
+                        bash $WORKSPACE/build-config/build-release-tools/post_test.sh \
+                        --type docker \
+                        --RackHDDir $WORKSPACE/build \
+                        --buildRecord $WORKSPACE/build_record
 
-            timeout(90){
-                sh '''#!/bin/bash +xe
-
-                #for test
-                git clone https://github.com/changev/rackhd
-                cd rackhd
-                git checkout feature/docker-compose-pull-file-image
-                cd ..
-                #
-
-                cd build/docker
-
-                bash $WORKSPACE/build-config/build-release-tools/post_test.sh \
-                --type docker \
-                --RackHDDir $WORKSPACE/rackhd \
-                --buildRecord $WORKSPACE/build_record
-                '''
+                        echo $SUDO_PASSWORD |sudo -S chown -R $USER:$USER $WORKSPACE/rackhd               
+                        '''
+                    }
+                }
             }
         }
     }
