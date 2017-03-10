a55cd8a51ed8927fe8e548986f245a2325ec2b81
pengtian0 <Tian.Peng@emc.com>
2017-03-10 00:33:10 -0500

    lock docker slave

--- jobs/build_docker/docker_post_test.groovy
+++ jobs/build_docker/docker_post_test.groovy
@@ -1,25 +1,22 @@
 node(build_docker_node){
-    lock("docker"){
-        timestamps{
-            withEnv(["WORKSPACE=${env.DOCKER_WORKSPACE}"]){
-                dir("build-config"){
-                    checkout scm
-                }
-                withCredentials([
-                    usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
-                                     passwordVariable: 'SUDO_PASSWORD',
-                                     usernameVariable: 'SUDO_USER')]
-                ){
-                    timeout(90){
-                        sh '''#!/bin/bash +xe
-                        bash $WORKSPACE/build-config/build-release-tools/post_test.sh \
-                        --type docker \
-                        --RackHDDir $WORKSPACE/build \
-                        --buildRecord $WORKSPACE/build_record
-
-                        echo $SUDO_PASSWORD |sudo -S chown -R $USER:$USER $WORKSPACE/build 
-                        '''
-                    }
+    timestamps{
+        withEnv(["WORKSPACE=${env.DOCKER_WORKSPACE}"]){
+            dir("build-config"){
+                checkout scm
+            }
+            withCredentials([
+                usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
+                                 passwordVariable: 'SUDO_PASSWORD',
+                                 usernameVariable: 'SUDO_USER')]
+            ){
+                timeout(90){
+                    sh '''#!/bin/bash +xe
+                    bash $WORKSPACE/build-config/build-release-tools/post_test.sh \
+                    --type docker \
+                    --RackHDDir $WORKSPACE/build \
+                    --buildRecord $WORKSPACE/build_record
+                    echo $SUDO_PASSWORD |sudo -S chown -R $USER:$USER $WORKSPACE/build 
+                    '''
                 }
             }
         }
