b409f22d88496adff18017955c9c2c17fa02699d
Geoff Reid <geoffrey.reid@emc.com>
2017-03-21 15:01:31 -0400

    Revert "fix xml file list parsing"
    This reverts commit b98db2a1a418a224e60bff2f539ac691ca8b395a.

--- jobs/function_test/function_test.groovy
+++ jobs/function_test/function_test.groovy
@@ -72,11 +72,11 @@ def function_test(String test_name, String label_name, String TEST_GROUP, Boolea
                         sh '''#!/bin/bash -x
                         mkdir '''+"$artifact_dir"+'''
                         ./build-config/post-deploy.sh
-                        files=$( ls build-deps/*.log | tr '\n' ' ' )
+                        files=$( ls build-deps/*.log )
                         if [ ! -z "$files" ];then
                             cp build-deps/*.log '''+"$artifact_dir"+'''
                         fi
-                        files=$( ls RackHD/test/*.xml | tr '\n' ' ' )
+                        files=$( ls RackHD/test/*.xml )
                         if [ ! -z "$files" ];then
                             cp RackHD/test/*.xml '''+"$artifact_dir" +'''
                         fi
@@ -85,7 +85,7 @@ def function_test(String test_name, String label_name, String TEST_GROUP, Boolea
  
                         sh '''#!/bin/bash -x
                         ./build-config/jobs/function_test/cleanup.sh
-                        files=$( ls RackHD/test/*.xml | tr '\n' ' ' )
+                        files=$( ls RackHD/test/*.xml )
                         if [ -z "$files" ];then
                             echo "No test result files generated, maybe it's aborted"
                             exit 1
