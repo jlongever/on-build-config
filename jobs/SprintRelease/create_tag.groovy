2d2117118d8e827a894f768e7ac49c3d79da0cce
pengtian0 <Tian.Peng@emc.com>
2017-04-25 17:45:13 -0500

    Single step sprint release version 2

--- jobs/SprintRelease/create_tag.groovy
+++ jobs/SprintRelease/create_tag.groovy
@@ -1,8 +1,7 @@
 node{
     withEnv([
         "tag_name=${tag_name}",
-        "JUMP_VERSION=${JUMP_VERSION}",
-        "IS_OFFICIAL_RELEASE=${env.IS_OFFICIAL_RELEASE}",
+        "MANIFEST_FILE_URL=${env.MANIFEST_FILE_URL}",
         "BINTRAY_SUBJECT=${env.BINTRAY_SUBJECT}",
         "BINTRAY_REPO=binary"])
     {
@@ -19,68 +18,10 @@ node{
                              usernameVariable: 'BINTRAY_USERNAME')
         ]){
             int retry_times = 3
-            if(JUMP_VERSION == "true"){
-                stage("Jump Version"){
-                    withEnv(["MANIFEST_FILE_URL=${MANIFEST_FILE_URL}"]){
-                        retry(retry_times){
-                            timeout(5){
-                                sh '''#!/bin/bash -e
-                                set +x
-                                export GITHUB_CREDS=$JENKINSRHD_GITHUB_CREDS
-                                set -x
-                                # Checkout code according to manifest file
-                                curl --user $BINTRAY_USERNAME:$BINTRAY_API_KEY -L "$MANIFEST_FILE_URL" -o rackhd-manifest
-                                ./build-config/build-release-tools/HWIMO-BUILD build-config/build-release-tools/application/reprove.py \
-                                --manifest rackhd-manifest \
-                                --builddir b \
-                                --git-credential https://github.com,GITHUB_CREDS \
-                                --jobs 8 \
-                                --force \
-                               checkout
-                               '''
-                            }
-                        }
-
-                        retry(retry_times){
-                            timeout(3){
-                                sh '''#!/bin/bash -e
-                                set +x
-                                export GITHUB_CREDS=$JENKINSRHD_GITHUB_CREDS
-                                set -x
-                                # Jump version of repositories which are just checked out.
-                                version=`echo $tag_name | grep "[0-9.]*" -o`
-                                ./build-config/build-release-tools/HWIMO-BUILD build-config/build-release-tools/application/jump_version.py \
-                                --build-dir b \
-                                --version $version \
-                                --message "release $version" \
-                                --publish \
-                                --git-credential https://github.com,GITHUB_CREDS
-                                '''
-                            }
-                        }
-                        retry(retry_times){
-                            timeout(3){
-                                sh './build-config/jobs/SprintRelease/update_manifest.sh'
-                            }
-                        }
-                        // inject properties file as environment variables
-                        if(fileExists ('downstream_file')) {
-                            def props = readProperties file: 'downstream_file';
-                            if(props['MANIFEST_FILE_URL']) {
-                                env.MANIFEST_FILE_URL = "${props.MANIFEST_FILE_URL}";
-                            }
-                        }
-                    }
-                }
-            }
-
             stage("Create Tag"){
-                withEnv(["MANIFEST_FILE_URL=${env.MANIFEST_FILE_URL}"])
-                {
-                    retry(retry_times){
-                        timeout(5){
-                            sh './build-config/jobs/SprintRelease/create_tag.sh'
-                        }
+                retry(retry_times){
+                    timeout(5){
+                        sh './build-config/jobs/SprintRelease/create_tag.sh'
                     }
                 }
             }
