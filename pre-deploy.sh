cc58f2dd6789decf5cfde79ecd5c6b4c8c20cc44
lanchongyizu <lanchongyizu@gmail.com>
2017-05-18 03:52:21 -0400

    Fix: RAC-5054: Timed out waiting for RackHD API service
    1) Change the synced folder type of /home/vagrant/src from VirtualBox shared folders to RSync
    2) Create directory /home/vagrant/log in guest machine to sync ${WORKSPACE}/build-log in host machine
    3) Redirect all logs to /home/vagrant/log and ${WORKSPACE}/build-log
    
    The link of the RAC is https://rackhd.atlassian.net/browse/RAC-5054
    
    After testing `vagrant up` on Jenkins Slave in Shanghai with different types, times for `waitForAPI` are shown below.
    These data are just for reference.
    * the VirtualBox shared folder: ~130s
    * the VirtualBox shared folder and copy another src for use: ~30s
    * NFS: ~40s
    * RSync: ~10s

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Running pre-deploy script"
 
-#raw sol logs saved in WORKSPACE/build-deps
+#raw sol logs saved in WORKSPACE/build-log
 #this script convert them into html by ansi2html tool
-#and move them to 'build' folder for publishing
-cd $WORKSPACE/build-deps
+#and move them to 'build-log' folder for publishing
+cd $WORKSPACE/build-log
 for file in `ls *sol.log.raw`; do
-    ansi2html < $file > $WORKSPACE/build/${file%.*}
+    ansi2html < $file > $WORKSPACE/build-log/${file%.*}
 done
+
