c190c565ca2cabf0e66e8b3be99a51bd2ff87316
Peter Pan <Peter.Pan@emc.com>
2017-07-02 21:14:50 -0400

    Migrate ATLAS to Vagrant Cloud - app.vagrantup.com

--- jobs/release/release_vagrant.sh
+++ jobs/release/release_vagrant.sh
@@ -4,7 +4,7 @@ echo "upload vagrant to atlas"
 
 ./on-build-config/build-release-tools/HWIMO-BUILD on-build-config/build-release-tools/application/release_to_atlas.py \
 --build-directory ./VAGRANT/build/packer \
---atlas-url https://atlas.hashicorp.com/api/v1 \
+--atlas-url https://app.vagrantup.com/api/v1  \
 --atlas-creds ${ATLAS_CREDS} \
 --atlas-name rackhd \
 --is-release $IS_OFFICIAL_RELEASE 
