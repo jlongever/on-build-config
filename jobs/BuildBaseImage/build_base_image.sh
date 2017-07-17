d81a024c63d45fe0c2a527f53ba90066c398e050
PengTian0 <Tian.Peng@emc.com>
2017-07-17 04:46:58 -0400

    push docker image to dockerhub

--- jobs/BuildBaseImage/build_base_image.sh
+++ jobs/BuildBaseImage/build_base_image.sh
@@ -2,7 +2,10 @@
 
 pushd $WORKSPACE/on-build-config/jobs/BuildBaseImage/base_docker
 echo $SUDO_PASSWORD |sudo -S docker build -t rackhd/pipeline .
+echo $SUDO_PASSWORD |sudo -S docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWD
+echo $SUDO_PASSWORD |sudo -S docker push rackhd/pipeline:latest
 popd
+
 pushd $WORKSPACE
 echo $SUDO_PASSWORD |sudo -S docker save -o rackhd_pipeline_docker.tar rackhd/pipeline
 echo $SUDO_PASSWORD |sudo -S chown $USER:$USER rackhd_pipeline_docker.tar
