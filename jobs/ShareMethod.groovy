9c19fc2ddb2ea27735f1fc13f1616065a4a499e6
PengTian0 <Tian.Peng@emc.com>
2017-07-27 01:14:16 -0400

    Make post result as a shared method to support independent repository PR Gate

--- jobs/ShareMethod.groovy
+++ jobs/ShareMethod.groovy
@@ -200,4 +200,47 @@ def downloadManifest(String url, String target){
     }
 }
 
+def generateManifestFromPR(String manifest_path){
+    def manifest = new net.sf.json.JSONObject()
+    String repository_url = "https://github.com/${ghprbGhRepository}.git"
+    manifest.putAll([
+        "repositories":[
+                [
+                "branch":"",
+                "commit-id":sha1,
+                "repository":repository_url,
+                "under-test": true
+                ]
+            ]
+        ])
+    writeJSON file: manifest_path, json: manifest   
+}
+
+def writeBackToGitHub(String library_dir, String manifest_path){
+    try{
+        // if previous steps all pass,  $currentBuild.result will be set to "SUCCESS" explictly in pipeline groovy code
+        // if Junit plugin found test case error in previous step,  the plugin will set $currentBuild.result  to "Unstable"
+        // if previous steps abort with error, the $currentBuild.result will not get chance to be set . so value is "null" here
+        // ------
+        //Jenkins currentBuild.result| github commit status(https://developer.github.com/v3/repos/statuses/ )
+        // null                      | failure
+        // failure                   | failure
+        // unstable                  | failure
+        // success                   | success
+        if ("${currentBuild.result}" != "SUCCESS"){
+            currentBuild.result = "FAILURE"
+        }
+        withCredentials([string(credentialsId: 'JENKINSRHD_GITHUB_TOKEN',
+                        variable: 'GITHUB_TOKEN')]){
+            sh """#!/bin/bash -ex
+            pushd ${library_dir}
+            ./jobs/write_back_github/write_back_github.sh ${library_dir} ${manifest_path} ${currentBuild.result}
+            popd
+            """
+        }
+        
+    } catch(error){
+        echo "Caught: ${error}"
+    }
+}
 return this
--- jobs/write_back_github/write_back_github.sh
+++ jobs/write_back_github/write_back_github.sh
@@ -1,20 +1,22 @@
 #!/bin/bash 
 
 set -x
-pushd $WORKSPACE
-build_status="${status}"
+build_config_dir="$1"
+manifest_path="$2"
+build_status="$3"
 
+pushd $1
 if [ "${build_status}" != "SUCCESS" ]; then
-    ./build-config/build-release-tools/HWIMO-BUILD ./build-config/build-release-tools/application/post-result.py \
-    --manifest_file "${stash_manifest_path}" \
+    ./build-release-tools/HWIMO-BUILD ./build-release-tools/application/post-result.py \
+    --manifest_file "${manifest_path}" \
     --jenkins_url "${JENKINS_URL}" \
     --build_url "${BUILD_URL}" \
     --public_jenkins_url "http://147.178.202.18/" \
     --ghtoken ${GITHUB_TOKEN}
 fi
 
-./build-config/build-release-tools/HWIMO-BUILD ./build-config/build-release-tools/application/commit_status_setter.py \
---manifest "${stash_manifest_path}" \
+./build-release-tools/HWIMO-BUILD ./build-release-tools/application/commit_status_setter.py \
+--manifest "${manifest_path}" \
 --build-url "${BUILD_URL}" \
 --public-jenkins-url "http://147.178.202.18/" \
 --status "${build_status,,}" \
